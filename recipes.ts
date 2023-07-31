5a96ce1e3b6f61a5bc6772b80ffce658d962117c
Valeriia Klestova <valeriya.apukhtina@gmail.com>
2023-07-31 22:25:44 +0200

    created auth directory with script to auth a user, added /login endpoint to authenticate a user; added /recipes/my endpoint and a get API request to fetch recipes of the authenticated user; to calculate rating for each user's recipe created a script ratedRecipe which can be imported and used for any API request

--- recipes.ts
+++ recipes.ts
@@ -1,9 +1,56 @@
+import { Prisma, PrismaClient } from "@prisma/client";
 import { Router } from "express";
+import AuthMiddleware, { AuthRequest } from "./auth/middleware";
+import ratedRecipe from "./services/ratedRecipe";
 
 export const recipeRouter = Router();
+const prisma = new PrismaClient();
 
 recipeRouter.get("/", (req, res) => {
   res.send("Hello from recipe router");
+});
+
+recipeRouter.get("/show/my", AuthMiddleware, async (req: AuthRequest, res) => {
+  const userId = req.userId;
+  // request to a database to fetch a JSON with the data we want to display + id for further modifications
+  const myRecipes = await prisma.recipe.findMany({
+    where: {
+      userId: userId,
+    },
+    select: {
+      id: true,
+      name: true,
+      prepTime: true,
+      imgUrl: true,
+      serves: true,
+    },
+  });
+  // creating an array of recipeIds we fetched for a specific user
+  const recipeIds: number[] = [];
+  for (let aRecipe of myRecipes) {
+    recipeIds.push(aRecipe.id);
+  }
+  // calling the function that return an array of objects with recipeId and calculated rating for each recipe
+  const recipeIdRating = await ratedRecipe(recipeIds);
+  // creating an array of objects we want to return to a client, collecting all necessary information
+  const ratedRecipes = [];
+  for (let i = 0; i < myRecipes.length; i++) {
+    const ratingInfo = recipeIdRating.find(
+      (anObj) => anObj.recipeId === myRecipes[i].id
+    );
+    ratedRecipes.push({
+      name: myRecipes[i].name,
+      prepTime: myRecipes[i].prepTime,
+      imgUrl: myRecipes[i].imgUrl,
+      serves: myRecipes[i].serves,
+      recipeRating: !ratingInfo
+        ? 0
+        : Math.floor(
+            ratingInfo["_avg"]["rating"] ? ratingInfo["_avg"]["rating"] : 0
+          ),
+    });
+  }
+  res.send(ratedRecipes);
 });
 
 recipeRouter.get("/:id", (req, res) => {
