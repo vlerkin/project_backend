f82a208e893f04bb50f8113596fccd7ed2a366bc
Valeriia Klestova <valeriya.apukhtina@gmail.com>
2023-08-01 20:22:43 +0200

    added CORS to enable a next.js client making requests to the server; added imgUrls for recipes in json for seeding

--- server.ts
+++ server.ts
@@ -8,6 +8,14 @@ import { toToken } from "./auth/jwt";
 // Create an express app
 const app = express();
 
+// CORS headers
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 // Tell the app to allow json in the request body
 app.use(json());
 
